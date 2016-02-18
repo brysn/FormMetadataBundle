353e9601b88be264757af3371f42caae5fc24141
Bryson Armstrong <bryson@kasacapital.com>
2016-02-17 19:44:00 -0800

    Using a form extension so that symfony's form factory can create the form and the form mapper is no longer needed.

---
+++ DependencyInjection/Compiler/FormExtensionCompilerPass.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace FlintLabs\Bundle\FormMetadataBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+class FormExtensionCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->has('form.registry')) {
+            return;
+        }
+
+        $definition = $container->findDefinition('form.registry');
+        $extensions = $definition->getArgument(0);
+        $extensions[] = new Reference('form_metadata.extension');
+        $definition->replaceArgument(0, $extensions);
+    }
+}
---
+++ FormExtension.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace FlintLabs\Bundle\FormMetadataBundle;
+
+use FlintLabs\Bundle\FormMetadataBundle\Driver\MetadataDriverInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Form\AbstractExtension;
+use Symfony\Component\Form\Exception\InvalidArgumentException;
+
+class FormExtension extends AbstractExtension
+{
+    /**
+     * Drivers that will be used to obtaining metadata
+     * @var MetadataDriverInterface[]
+     */
+    private $drivers = array();
+
+    public function getType($name)
+    {
+        $metadata = $this->getMetadata($name);
+        if (!$metadata) {
+            throw new InvalidArgumentException(sprintf('The type "%s" can not be loaded by this extension', $name));
+        }
+
+        return new FormType($metadata);
+    }
+
+    public function hasType($name)
+    {
+        $metadata = $this->getMetadata($name);
+
+        return $metadata ? true : false;
+    }
+
+    /**
+     * Add an entity metadata reader to the readers
+     * @param MetadataDriverInterface $driver
+     * @return void
+     */
+    public function addDriver(MetadataDriverInterface $driver)
+    {
+        $this->drivers[] = $driver;
+    }
+
+    protected function getMetadata($name)
+    {
+        // Look to the readers to find metadata
+        foreach ($this->drivers as $driver) {
+            $metadata = $driver->getMetadata($name);
+            if ($metadata) {
+                $fields = $metadata->getFields();
+                if (!empty($fields)) {
+                    return $metadata;
+                }
+            }
+        }
+
+        return null;
+    }
+}
